8bfd2eab6690cfbb11c682434cc030c8957eafc7
GitHub Action <action@github.com>
2023-02-15 22:25:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/frandemona/timetrackerintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/frandemona/timetrackerintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.frandemona.timetrackerintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frandemona.timetrackerintellij.MyBundle
 
 class MyApplicationService {
 
